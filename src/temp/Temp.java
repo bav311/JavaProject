4a7f89da0b16d487b99738225391a182854806be
“bav” <bav311@yandex.ru>
2022-01-18 20:58:58 +0500

    arr - scr

--- src/temp/Temp.java
+++ src/temp/Temp.java
@@ -2,8 +2,15 @@ package temp;
 
 public class Temp {
     public static void main(String[] args) {
-        System.out.println("Hi");
+        int n = 100;
+        int []arr = new int [n];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i]= (int) (Math.random() * (20+1)-10); // -10 +10
+        }
 
+        for (int i:arr) {
+            System.out.print(i + " ");
 
+        }
     }
 }
