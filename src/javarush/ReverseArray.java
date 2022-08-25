6ff2ef0990107e9b59cdc3aca08e609bd6177c4d
Андрей <bav311@yandex.ru>
2022-08-25 14:04:53 +0500

    ReverseArray

---
+++ src/javarush/ReverseArray.java
@@ -0,0 +1,45 @@
+/*Напишем утилиту для работы с массивами. Основная часть функционала готова:
+метод printArray() выводит в консоли все элементы массива.
+Тебе осталась мелочь: реализовать метод reverseArray().
+Он должен менять порядок элементов массива на обратный.
+Метод должен работать только с массивами целочисленных значений (int[]).
+
+Пример:
+Если массив содержал элементы:
+1, 2, 3, 4, 5, 6, 7, 8, 9, 0
+то после вызова метода reverseArray() должен содержать:
+0, 9, 8, 7, 6, 5, 4, 3, 2, 1
+
+Требования:
+Метод reverseArray() должен менять порядок элементов массива на обратный.*/
+
+package javarush;
+
+
+class ReverseArray {
+    public static void main(String[] args) {
+        int[] array = {11, 12, 13, 14, 15, 16, 17, 18, 19, 110};
+        printArray(array);
+        reverseArray(array);
+        printArray(array);
+
+    }
+
+    public static void printArray(int[] array) {
+        for (int i : array) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+
+    public static void reverseArray(int[] array) {
+        for (int i = 0; i < array.length / 2; i++) {
+            int tmp = array[i];
+            array[i] = array[array.length-i-1];
+            array[array.length-i-1] = tmp;
+
+
+        }
+
+    }
+}
