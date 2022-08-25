a3cfc964d92e5db336af9c8c364f1f60d7b06956
“bav” <bav311@yandex.ru>
2022-08-25 23:02:24 +0500

    Update ChessBoard.java

--- src/javarush/ChessBoard.java
+++ src/javarush/ChessBoard.java
@@ -1,21 +1,22 @@
-/*
-Реализуй метод main(String[]), который выводит в консоль true, если элемент содержится
-в переданном массиве, иначе — false.
-Массив array не должен изменять расположение своих элементов.
-Для поиска элемента в массиве нужен бинарный поиск. Чтобы это сделать,
-используй статический метод Arrays.binarySearch(int[], int) класса Arrays.
-Первым параметром нужно передать отсортированный массив, в котором выполнить
-поиск, вторым параметром — искомый элемент. Метод Arrays.binarySearch(int[], int)
-возвращает индекс искомого элемента, если такой элемент есть в массиве.
-Для сортировки массива можно использовать метод Arrays.sort(int[]).
-При тестировании значения переменных класса Solution будут разными.*/
 package javarush;
 public class ChessBoard {
     public static String chessBoardCord(int a, int b) {
         String letters = "abcdefgh";
         String numbers = "87654321";
         if ((a > 7)|| (b>7)) return null; //если номер за пределами доски, возвращаем значение по умолчанию - null
-        else return (Character.toString(letters.charAt(a)) + numbers.charAt(b)); /*charAt - метод, с помощью которого мы извлекаем из строки элемент под переданным номером, здесь - под номерами a и b. Character.toString - метод, который переводит полученный символ в строку*/
+        else return (Character.toString(letters.charAt(a)) + numbers.charAt(b));
+        /*charAt - метод, с помощью которого мы извлекаем из строки элемент под переданным номером,
+        здесь - под номерами a и b. Character.toString - метод, который переводит полученный символ
+        в строку*/
+    }
+    public static void printArray(String[][] chessBoard) {
+        for (String[] strings : chessBoard) {
+            for (String string : strings) {
+                System.out.print(string + "\t");
+
+            }
+            System.out.println();
+        }
     }
 
     public static void main(String[] args) {
@@ -28,15 +29,10 @@ public class ChessBoard {
             }
         }
 
-        for (String[] strings : chessBoard) {
-            for (String string : strings) {
-                System.out.print(string + "\t");
-
-            }
-            System.out.println();
-        }
-
+        printArray(chessBoard);
 
 
     }
+
+
 }
