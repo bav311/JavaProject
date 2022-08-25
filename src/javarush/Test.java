b8e558f964efe1c1bad07bf6ba4eb9144dca4416
“bav” <bav311@yandex.ru>
2022-08-25 22:58:42 +0500

    Update Test.java

--- src/javarush/Test.java
+++ src/javarush/Test.java
@@ -2,6 +2,8 @@ package javarush;
 
 public class Test {
     public static void main(String[] args) {
+        int[][] months = {{31, 28}, {31, 30, 31}, {30, 31, 31}};
+        new ArrayPrint(months);
 
     }
 }
