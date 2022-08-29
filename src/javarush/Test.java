e2d9479042c6ed538a041400e8d8997abebf2196
Andrew from home <bav311@yandex.ru>
2022-08-29 20:30:07 +0500

    1.txt ssh

--- src/javarush/Test.java
+++ src/javarush/Test.java
@@ -1,25 +1,15 @@
 package javarush;
 
 public class Test {
-    public static void main(String[] args) {
-        System.out.println(isDivisible(12,4,3));
-        System.out.println(isDivisible(3,3,4));
-       
+    public  String name = "Amigo";
+    public  String position = "Java developer";
+    public static int salary = 10_000;
 
-	   System.out.println(ninthDegree(2, 3));
-
-
+    public void setPosition(String position) {
+        this.position = position;
     }
 
-    public static boolean isDivisible(long n, long x, long y) {
-        return n % x == 0 && n % y == 0;
-    }
-
-    public static long ninthDegree(long num, long degree) {
-        int result = 1;
-        for (int i = 0; i < degree; i++) {
-            result *= num;
-        }
-        return result;
+    public void setSalary(int salary) {
+        this.salary = salary;
     }
 }
