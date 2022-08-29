d04ae5646a05f7fca997c5f3b303d936bc689a97
Andrew from Job <bav311@yandex.ru>
2022-08-29 14:41:34 +0500

    number in degree

---
+++ src/javarush/MinMax.java
@@ -0,0 +1,11 @@
+package javarush;
+
+public class MinMax {
+     public static int min (int x, int y) {
+        return x < y? x:y;
+    }
+
+    public static int max (int x, int y) {
+        return x>y? x:y;
+    }
+}
--- src/javarush/Test.java
+++ src/javarush/Test.java
@@ -2,8 +2,15 @@ package javarush;
 
 public class Test {
     public static void main(String[] args) {
-        int[][] months = {{31, 28}, {31, 30, 31}, {30, 31, 31}};
-        new ArrayPrint(months);
+        System.out.println(ninthDegree(2, 3));
 
+    }
+
+    public static long ninthDegree(long num, long degree) {
+        int result = 1;
+        for (int i = 0; i < degree; i++) {
+            result *= num;
+        }
+        return result;
     }
 }
