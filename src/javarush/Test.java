fd7e574d39301848ffe121f767a3792e8274fce9
Andrew from Job <bav311@yandex.ru>
2022-08-30 15:52:52 +0500

    deadMen

--- src/javarush/Test.java
+++ src/javarush/Test.java
@@ -1,15 +1,30 @@
+/*Замок чернобайта. Создай метод String destroyDeadMan(String), который будет принимать
+на вход "имя" мертвеца и возвращать фразу "<имя> был уничтожен",
+где <имя> — значение аргумента метода destroyDeadMan.
+В методе main вызови метод destroyDeadMan(String) для каждого мертвеца из списка deadMen,
+результат вызова выведи на экран.*/
+
 package javarush;
 
-public class Test {
-    public  String name = "Amigo";
-    public  String position = "Java developer";
-    public static int salary = 10_000;
 
-    public void setPosition(String position) {
-        this.position = position;
+public class Test {
+    public static String[] deadMen = {"мертвец_1", "мертвец_2", "мертвец_3", "мертвец_4", "мертвец_5",
+            "мертвец_6", "мертвец_7", "мертвец_8"};
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i < deadMen.length; i++) {
+            System.out.println(deadMen[i]);
+
+        }
+
     }
 
-    public void setSalary(int salary) {
-        this.salary = salary;
+     public static String destroyDeadMan(String name) {
+        name = "destroyd";
+         for (int i = 0; i < deadMen.length; i++) {
+         deadMen[i] = deadMen[i] + name;
+         }
+        return name;
     }
 }
