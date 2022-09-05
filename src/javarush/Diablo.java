feae2b6434102336f693f4e94464de57854c99cb
Andrew from Job <bav311@yandex.ru>
2022-09-05 15:58:06 +0500

    diabilo

---
+++ src/javarush/Diablo.java
@@ -0,0 +1,104 @@
+/*
+Теперь реализуем саму битву — метод battle(). Добавь в класс Solution
+публичный статический метод void battle. В этом методе Амиго будет атаковать,
+а Диабло — защищаться и контратаковать до тех пор,
+пока кто-то из них не лишиться всех "жизней".
+Если Амиго атаковал, а Диабло не защитился (зона защиты Диабло не совпала с зоной атаки Амиго),
+то Диабло теряет "жизни" — вызываем соответствующий метод.
+Если же Диабло защитился, он сразу же контратакует и Амиго теряет "жизнь" (снова вызываем нужный метод).
+
+Кроме этого мы будем выводить в консоль ход битвы: если Диабло смог защититься,
+то выводим фразу из переменной diabloDefendPhrase.
+Если же удар Амиго попал в цель, то выводим в консоль значение amigoAttackPhrase.
+Не забудь вызвать метод battle() в методе main после вызова метода findDiablo().
+
+Требования:
+В классе Solution должен быть публичный статический метод void battle().
+Метод battle() не должен ничего делать, если кто-то из соперников не имеет "жизней".
+Метод battle() должен вывести на экран значение поля diabloDefendPhrase
+и вызвать метод amigoLostLife(), в том случае,
+если Диабло защитился (amigoAttacks() == diabloDefends()).
+
+Метод battle() должен вывести на экран значение поля amigoAttackPhrase и
+вызвать метод diabloLostLife(), в том случае, если Диабло не смог защититься.
+В методе main нужно вызвать метод battle() после вызова метода findDiablo().*/
+
+package javarush;
+
+import java.util.Scanner;
+
+public class Diablo {
+    public static String getFirstPositionPhrase = "Помоги Амиго определить, где спрятался Диабло? Введи номер позиции (1, 2, 3 или 4):";
+    public static String getPositionPhrase = "Диабло не обнаружен. Давай попробуем снова. Введи номер позиции (1, 2, 3 или 4):";
+    public static String findDiabloPhrase = "Амиго обнаружил Диабло.";
+    public static String diabloDefendPhrase = "Амиго атакует. Диабло отбил атаку Амиго и нанес ответный удар.";
+    public static String amigoAttackPhrase = "Удар Амиго достиг цели. Диабло потерял 3 жизни.";
+    public static String winPhrase = "Амиго одержал победу над Диабло и выбил из его головы загадочный кристалл.";
+    public static String loosePhrase = "Диабло победил Амиго.";
+    public static int diabloPosition;
+    public static int amigoLives = 9;
+    public static int diabloLives = 9;
+
+    public static void main(String[] args) {
+        diabloPosition = getRandomNumber(4);
+
+        findDiablo();
+        battle();
+        System.out.println("amigoLives " + amigoLives);
+        System.out.println("diabloLives " + diabloLives);
+
+    }
+
+    public static int amigoLostLife() {
+        return amigoLives--;
+
+    }
+
+    public static void diabloLostLife() {
+        diabloLives -= 3;
+
+    }
+
+    public static int amigoAttacks() {
+        return (getRandomNumber(3));
+    }
+
+    public static int diabloAttacks() {
+        return (getRandomNumber(3));
+    }
+
+    public static int diabloDefends() {
+        return (getRandomNumber(3));
+    }
+
+    public static void findDiablo() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(getFirstPositionPhrase);
+        while (true) {
+            int position = scanner.nextInt();
+            if (position == diabloPosition) {
+                System.out.println(findDiabloPhrase);
+                break;
+            } else {
+                System.out.println(getPositionPhrase);
+            }
+        }
+
+    }
+
+    public static int getRandomNumber(int range) {
+        return (int) (Math.random() * range) + 1;
+    }
+
+    public static void battle() {
+        while ((amigoLives > 1) | (diabloLives > 3)) {
+            if (amigoAttacks() == diabloDefends()) {
+                amigoLostLife();
+                System.out.println(diabloDefendPhrase);
+            } else {
+                diabloLostLife();
+                System.out.println(amigoAttackPhrase);
+            }
+        }
+    }
+}
--- src/javarush/Test.java
+++ src/javarush/Test.java
@@ -1,30 +1,10 @@
-/*Создай метод switchSword(String) с параметром типа String,
-который будет выводить фразу "Меч переключен в <параметр> режим.",
-где <параметр> — значение аргумента метода switchSword.
-Вызови метод switchSword() с нужным параметром и в нужном месте метода main.
-Остальной код метода main не изменяй.*/
-
 package javarush;
 
-import java.util.Arrays;
 
 public class Test {
-    public static void fill(int[] data, int from, int to, int value)
-    {
-        if (from < 0 || to > data.length)
-            return;
+    public static void main(String[] args) {
+        System.out.println((int) (Math.random() * 4) + 1);
 
-        for (int i = from; i < to; i++)
-        {
-            data[i] = value;
-        }
     }
 
-    public static void main(String[] args)
-    {
-        int[] months = {1, 2, 3, 4, 5, 6, 7, 8 ,9, 10, 11, 12};
-        System.out.println(Arrays.toString(months));
-        fill(months, 2, 12, 8);
-        System.out.println(Arrays.toString(months));
-    }
 }
