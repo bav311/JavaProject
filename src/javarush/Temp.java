76efe83eba9cbd757dc4461ad1607ed86070f3e1
Andrew from Job <bav311@yandex.ru>
2022-10-20 14:34:07 +0500

    MagicBall

--- src/javarush/Temp.java
+++ src/javarush/Temp.java
@@ -1,11 +1,11 @@
+
+
 package javarush;
 
-
 public class Temp {
-
-
     public static void main(String[] args) {
+        System.out.println("Заработаю ли я себе на BMW?");
+        System.out.println(MagicBall.getPrediction());
 
     }
-
 }
