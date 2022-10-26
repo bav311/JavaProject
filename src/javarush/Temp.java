4328dec0050ef35aa49558b97bbccb79dea6eaba
Andrew from home <bav311@yandex.ru>
2022-10-26 22:20:20 +0500

    Степень двойки

--- src/javarush/Temp.java
+++ src/javarush/Temp.java
@@ -1,9 +1,11 @@
 package javarush;
-import ndr.OverloadDemo;
 
 public class Temp {
     public static void main(String[] args) {
+        System.out.println(getPowerOfTwo(3));
+    }
 
-
+    public static int getPowerOfTwo(int power) {
+        return 2 << (power - 1);
     }
 }
