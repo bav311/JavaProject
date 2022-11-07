eb20df0faefac0ce595415f2f49d46f38401770c
Andrew from Job <bav311@yandex.ru>
2022-11-07 15:38:51 +0500

    system number + hex

--- src/javarush/SystemNumber.java
+++ src/javarush/SystemNumber.java
@@ -1,23 +1,23 @@
-/*Публичный статический метод toOctal(int) должен переводить целое число,
-полученное в качестве входящего параметра, из десятичной системы счисления в восьмеричную.
-А публичный статический метод toDecimal(int) наоборот — из восьмеричной в десятичную.
-Методы работают только с положительными числами. Если входящий параметр меньше или равен 0,
-методы возвращают 0.
-*/
-
-
 package javarush;
 
 public class SystemNumber {
+
+    private static final String HEX = "0123456789ABCDEF";
+
     public static void main(String[] args) {
-        int dNumber = 21;
+        int dNumber = 1973;
         int oNumber = 25;
         int bNumber = 10101;
+        String hexNumber = "7B5";
         System.out.println("Десятичное число " + dNumber + " равно восьмеричному числу " + toOctal8(dNumber));
         System.out.println("Восьмеричное число " + oNumber + " равно десятичному числу " + toDecimal8(oNumber));
         System.out.println("Десятичное число " + dNumber + " равно двоичному числу " + toBinary2(dNumber));
         System.out.println("Двоичное число " + bNumber + " равно десятичному числу " + toDecimal2(bNumber));
+        System.out.println("Десятичное число " + dNumber + " равно 16-ричному числу " + toHex(dNumber));
+        System.out.println("16-ричное число " + hexNumber + " равно десятичному числу " + toDecimal(hexNumber));
+
     }
+
     public static int toOctal8(int decimalNumber) {
         if (decimalNumber < 0) {
             return 0;
@@ -32,6 +32,7 @@ public class SystemNumber {
         }
         return octalNumber;
     }
+
     public static int toDecimal8(int octalNumber) {
         if (octalNumber < 0) {
             return 0;
@@ -46,6 +47,7 @@ public class SystemNumber {
         }
         return decimalNumber;
     }
+
     public static int toBinary2(int decimalNumber) {
         if (decimalNumber < 0) {
             return 0;
@@ -60,6 +62,7 @@ public class SystemNumber {
         }
         return binaryNumber;
     }
+
     public static int toDecimal2(int binaryNumber) {
         if (binaryNumber < 0) {
             return 0;
@@ -73,5 +76,35 @@ public class SystemNumber {
             i++;
         }
         return decimalNumber;
-       }
+    }
+
+    public static String toHex(int decimalNumber) {
+        String nextNumber = "";
+        if (decimalNumber < 0) {
+            return nextNumber;
+        }
+        while (decimalNumber != 0) {
+            int i = decimalNumber % 16;
+            char ch = HEX.charAt(i);
+            nextNumber = ch + nextNumber;
+            decimalNumber /= 16;
+        }
+        return nextNumber;
+    }
+
+
+    public static int toDecimal(String hexNumber) {
+        int decimalNumber = 0;
+        if (hexNumber == null || hexNumber.equals("")) {
+            return decimalNumber;
+        }
+
+        for (int i = 0; i < hexNumber.length(); i++) {
+            char fromNexNumber = hexNumber.charAt(i);
+            int index = HEX.indexOf(fromNexNumber);
+            decimalNumber = 16 * decimalNumber + index;
+        }
+        return decimalNumber;
+    }
+
 }
--- src/javarush/Temp.java
+++ src/javarush/Temp.java
@@ -1,7 +1,46 @@
 package javarush;
 
 public class Temp {
+    private static final String HEX = "0123456789ABCDEF";
+
     public static void main(String[] args) {
+        int decimalNumber = 1973;
+        String hexNumber = "7B5";
+        System.out.println(decimalNumber + " = " + toHex(decimalNumber));
+
+        System.out.println(toDecimal(hexNumber));
+
 
     }
+
+    public static String toHex(int decimalNumber) {
+        String nextNumber = "";
+        if (decimalNumber < 0) {
+            return nextNumber;
+        }
+        while (decimalNumber != 0) {
+            int i = decimalNumber % 16;
+            char ch = HEX.charAt(i);
+            nextNumber = ch + nextNumber;
+            decimalNumber /= 16;
+        }
+        return nextNumber;
+    }
+
+
+    public static int toDecimal (String hexNumber) {
+        int decimalNumber = 0;
+        if (hexNumber == null || hexNumber.equals("")) {
+            return decimalNumber;
+        }
+
+        for (int i = 0; i < hexNumber.length(); i++) {
+            char fromNexNumber = hexNumber.charAt(i);
+            int index = HEX.indexOf(fromNexNumber);
+            decimalNumber = 16 * decimalNumber + index;
+        }
+        return decimalNumber;
+    }
+
+
 }
