0ffb00f002344091eeb2c32e3260c5b602c62e76
Andrew from home <bav311@yandex.ru>
2022-12-05 21:53:12 +0500

    StringTokenizer 09-05

--- src/javarush/Test.java
+++ src/javarush/Test.java
@@ -1,22 +1,42 @@
-//https://javarush.com/help/83528
+//ЗАДАЧА https://javarush.com/help/83528
+/*Используя StringTokenizer раздели query на части по разделителю delimiter.
+
+Пример:
+getTokens("java.util.stream", "\\.")
+возвращает массив строк
+{"java", "util", "stream"}
+
+Hint: решить задачу поможет метод countTokens().
+
+Требования:
+Нужно, чтобы метод getTokens(String, String) использовал StringTokenizer.
+Нужно, чтобы метод getTokens(String, String) возвращал массив типа String,
+заполненный согласно условию задачи.*/
 package javarush;
 
+
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Test {
 
 
     public static void main(String[] args) {
-        String str = "Good news everyone!";
+        String packagePath = "java.util.stream";
 
-        StringTokenizer tokenizer = new StringTokenizer(str,"e");
-        while (tokenizer.hasMoreTokens())
-        {
-            String token = tokenizer.nextToken();
-            System.out.println(token);
-        }
-
-
+        String[] tokens = getTokens(packagePath, "\\.");
+        System.out.println(Arrays.toString(tokens));
     }
 
+    public static String[] getTokens(String query, String delimiter) {
+        StringTokenizer st = new StringTokenizer(query, delimiter);
+        String[] strings = new String[st.countTokens()];
+        while (st.hasMoreTokens()) {
+            for (int i = 0; i < strings.length; i++) {
+                strings[i] = st.nextToken();
+            }
+        }
+
+        return strings;
+    }
 }
