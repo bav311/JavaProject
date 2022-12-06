543bf95a0673b81ad8e50281d1f83ebc2875dbd5
Andrew from Job <bav311@yandex.ru>
2022-12-06 15:30:03 +0500

    splitArrayElements и getStringFromArray

--- src/javarush/One.java
+++ src/javarush/One.java
@@ -1,21 +1,54 @@
+/*https://javarush.com/help/85733*/
 
+/*В блоки управления пиратов (массив controlUnits) проникли вирусы и баги.
+Требуется почистить эти блоки, чтобы помочь пиратам.
+
+С этой задачей нам помогут два метода в классе Solution:
+splitArrayElements и getStringFromArray.
+
+В методе splitArrayElements требуется пройтись по массиву controlUnits и
+с помощью StringTokenizer разбить элементы таким образом,
+чтобы массив изменился на пять следующих строк: "one", "two", "three", "four" и "five".
+
+Затем в методе getStringFromArray требуется пройтись по массиву controlUnits и
+с помощью String.format() отформатировать каждый его элемент таким образом,
+чтобы элементы массива изменились на следующие:
+
+"Block one back online."
+"Block two back online."
+"Block three back online."
+"Block four back online."
+"Block five back online."
+Метод main не участвует в проверке.
+
+Требования:
+Не изменяй изначальные значения массива controlUnits в классе Solution.
+Метод splitArrayElements должен изменять элементы массива controlUnits согласно условиям.
+Метод getStringFromArray должен изменять элементы массива controlUnits согласно условиям.*/
 
 package javarush;
 
+import java.util.StringTokenizer;
 
 public class One {
+    public static String[] controlUnits = {"iAm/A/spyware/one", "iAm/A/malware/two", "iAm/A/virus/three", "iAm/A/trojan/four", "iAm/bug/five"};
+
     public static void main(String[] args) {
-        String path = "/usr/java/jdk1.8/bin/";
-        String jdk13 = "jdk-13";
-        System.out.println(changePath(path, jdk13));
+//        splitArrayElements();
+//        getStringFromArray();
+        print(controlUnits);
+
     }
 
-    public static String changePath(String inLine1, String version1) {
-        int index1 = inLine1.indexOf("jdk");
-        int index2 = inLine1.indexOf("/", index1);
-        String regex1 = inLine1.substring(index1,index2);
-        return inLine1.replaceAll(regex1,version1);
+    public static String[] outStrings(String[] inStrings) {
+        StringTokenizer tokenizer = new StringTokenizer("fsdf", "\\.");
+
+        return null;
+    }
+
+    public static void print(String[] array) {
+        for (String a : array) {
+            System.out.println(a);
+        }
     }
 }
-
-
---
+++ src/javarush/string/TokenString.java
@@ -0,0 +1,42 @@
+//ЗАДАЧА https://javarush.com/help/83528
+/*Используя StringTokenizer раздели query на части по разделителю delimiter.
+
+Пример:
+getTokens("java.util.stream", "\\.")
+возвращает массив строк
+{"java", "util", "stream"}
+
+Hint: решить задачу поможет метод countTokens().
+
+Требования:
+Нужно, чтобы метод getTokens(String, String) использовал StringTokenizer.
+Нужно, чтобы метод getTokens(String, String) возвращал массив типа String,
+заполненный согласно условию задачи.*/
+package javarush.string;
+
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class TokenString {
+
+    public static void main(String[] args) {
+
+        String packagePath = "java.util.stream";
+
+
+        String[] tokens = getTokens(packagePath, "\\.");
+        System.out.println(Arrays.toString(tokens));
+    }
+
+    public static String[] getTokens(String query, String delimiter) {
+        StringTokenizer st = new StringTokenizer(query, delimiter);
+        String[] strings = new String[st.countTokens()];
+        while (st.hasMoreTokens()) {
+            for (int i = 0; i < strings.length; i++) {
+                strings[i] = st.nextToken();
+            }
+        }
+
+        return strings;
+    }
+}
